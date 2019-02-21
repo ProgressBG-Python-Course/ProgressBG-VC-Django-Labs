a56fefdcddad9338f289c75f1ec8b541124165d3
Iva E. Popova <progressbg.python.course@gmail.com>
2019-02-21 13:17:52 +0200

    prelab demos

--- labs/todo_project/todo_project/settings.py
+++ labs/todo_project/todo_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'todo_app',
+    'demos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- labs/todo_project/todo_project/urls.py
+++ labs/todo_project/todo_project/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('todos/', include('todo_app.urls')),
+    path('demos/', include('demos.urls')),
     path('admin/', admin.site.urls),
 ]
