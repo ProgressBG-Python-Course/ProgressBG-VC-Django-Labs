afbc897d031081fc552fc3a35e8ccd326bda4025
Iva E. Popova <progressbg.python.course@gmail.com>
2019-02-26 18:00:48 +0200

    Lab7: 26.02.2017 DONE: Static resources; Routing intro NEXT: Routing

--- labs/todo_project/todo_app/views.py
+++ labs/todo_project/todo_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # from django.template import loader
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from todo_app.models import Task
 
 # Create your views here.
@@ -18,16 +18,15 @@ def index(request):
 
 # http://127.0.0.1:8000/list
 def list(request):
-    latest_tasks = Task.objects.order_by('due')[:5]
-
-    context = {
-        'latest_tasks': latest_tasks
-    }
-
+    import datetime
+    
+    now = datetime.datetime.now()
+    
     template_file = 'todo_app/list.html'
 
+    # return HttpResponse('OK')
 
-    return render(request, template_file, context)
+    return render(request, template_file, {'now':now})
 
 def table(request):
     latest_tasks = Task.objects.order_by('due')[:5]
