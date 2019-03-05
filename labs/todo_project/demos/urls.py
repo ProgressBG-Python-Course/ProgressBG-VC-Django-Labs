0ff8f30320eab52de80706cc883f110d076cee46
Iva E. Popova <progressbg.python.course@gmail.com>
2019-03-05 16:28:33 +0200

    check TODO

--- labs/todo_project/demos/urls.py
+++ labs/todo_project/demos/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -6,5 +6,23 @@ urlpatterns = [
     path('list', views.list, name='list'),
     path('table', views.table, name='table'),
     path('task/<int:id>', views.task, name='task'),
+
+    # http://127.0.0.1:8000/demos/user/ivan
+    # http://127.0.0.1:8000/demos/user/ivan/ivanov
+    # => "Hello ivan"
+
+    # http://127.0.0.1:8000/demos/user/1
+    # => 'Hello ivan'
+
+   
+
+    # http://127.0.0.1:8000/demos/user/ivan/ivanov
+    
+    # http://127.0.0.1:8000/demos/user/ivan
+    # http://127.0.0.1:8000/demos/user/1
+    
+    # TODO: give examples with capturing groups with pure Python regex
+    re_path('user/(?:([A-Za-z]+)|(\d+))$', views.user),   
+    
 ]
 				
--- labs/todo_project/demos/views.py
+++ labs/todo_project/demos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # from django.template import loader
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from todo_app.models import Task
 
 # Create your views here.
@@ -41,3 +41,31 @@ def task(request, id):
 
 
     return render(request, template_file, context)
+
+
+
+def user(request,*args, **kwargs):
+    # http://127.0.0.1:8000/demos/user/name/surname
+    # http://127.0.0.1:8000/demos/user/user_id
+    # => Hello <user name>
+
+    # name = 'ivan'
+    # surname = 'ivanov'
+
+    # http://127.0.0.1:8000/demos/user/1
+    # id=1
+
+
+    'ivan/ivanov'
+    users = {
+        '1':{
+            'name': 'ivan',
+            'surname':'ivanov'
+        }
+    }
+
+    return HttpResponse(f"""
+        args: {args}<br>
+        kwargs: {kwargs}<br>
+        
+    """)
