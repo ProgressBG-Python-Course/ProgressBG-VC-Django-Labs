6d5bdcb1aced83dd0ec80b4b636618ee03100be5
Iva E. Popova <progressbg.python.course@gmail.com>
2019-03-07 13:44:59 +0200

    pre: Lab10: forms_demo

--- labs/todo_project/todo_project/urls.py
+++ labs/todo_project/todo_project/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   # 127.0.0.1:8000/todos
   path('todos/', include('todo_app.urls')),
   path('demos/', include('demos.urls')),
+  path('forms_demo/', include('forms_demo.urls')),
   path('admin/', admin.site.urls),
 ]
