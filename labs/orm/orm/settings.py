9c2a3eade554aaf08a271db602d5d52cd3a99533
Iva E. Popova <progressbg.python.course@gmail.com>
2019-03-21 00:13:40 +0200

    init: Lab14

--- labs/orm/orm/settings.py
+++ labs/orm/orm/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'models_overview',
+    # 'pure_SQL_demo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    }    
 }
 
 
---
+++ labs/orm/pure_SQL_demo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PureSqlDemoConfig(AppConfig):
+    name = 'pure_SQL_demo'
