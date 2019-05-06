a0750f41d835cf0959362a8166b77d7e00844c85
Iva E. Popova <progressbg.python.course@gmail.com>
2019-05-06 21:45:51 +0300

    register models in admin backend

--- labs/using_legacy_db/music_catalog_app/admin.py
+++ labs/using_legacy_db/music_catalog_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Album)
+admin.site.register(models.Artist)
+admin.site.register(models.Genre)
+admin.site.register(models.Track)
+admin.site.register(models.AlbumArtist)
+admin.site.register(models.AlbumGenre)
+admin.site.register(models.TrackArtist)
